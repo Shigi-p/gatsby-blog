4a0b4aec5d38067bc4613edb78785783c4e9e459
Shigi-p <shigi.522@gmail.com>
2024-01-04 23:18:53 +0900

    [remove]  remve unnecessary files
    yarnを使用するようにしたのでpackage-lock.jsonも削除した

--- make_summary_format.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-
-touch `date "+%Y%m%d"`.md
-echo '---\ntitle: summary_' `date "+%Y%m%d" ` ' \n---\n\n#' `date "+%Y%m%d"` >> `date "+%Y%m%d"`.md
--- md.sh
+++
@@ -1,9 +0,0 @@
-
-
-for file in `find $HOME/Develop/ -type d -path \*/summary_tech 2>/dev/null`
-do
-  echo $file
-  ls $file
-  echo $file/*.md $HOME/Develop/summary_tech_shigi/my_tech_blog/src/md_pages/
-  cp $file/*.md $HOME/Develop/summary_tech_shigi/my_tech_blog/src/md_pages/
-done
